51112d05301b13c2fc2af471c03f1a4078208757
cailiudel@gmail.com <caili@udel.edu>
2022-08-26 02:45:35 -0700

    more ui

--- mesh_gen_2.py
+++ mesh_gen_2.py
@@ -12,9 +12,14 @@ import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
 from matplotlib.widgets import RectangleSelector
 from solidspy.solids_GUI import solids_GUI
-import pygame_gui
 from collections import deque
 import tkinter
+import time
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.backends.backend_agg as agg
+import pylab
 
 SCALE = 50
 
@@ -112,25 +117,181 @@ class UIPanel:
   def __init__(self):
     self.root = tkinter.Tk()
     self.root.title("boxingfem Config")
-    self.root.geometry('200x600')
+    self.root.geometry('260x600')
     self.root.protocol("WM_DELETE_WINDOW", quit_callback)
 
     self.showOriginal = tkinter.BooleanVar()
     self.showOriginal.set(True)
 
-    tkinter.Label(self.root, text="Layers").grid(row=0, column=0, sticky=tkinter.W)
-    tkinter.Checkbutton(self.root, text="Original Image", variable = self.showOriginal, onvalue = True, offvalue = False).grid(row=1, column=0, sticky=tkinter.W)
+    self.showPatches = tkinter.BooleanVar()
+    self.showPatches.set(True)
+
+    tkinter.Label(self.root, text="Layers").grid(row=0, column=0, sticky=tkinter.W, columnspan=2)
+    tkinter.Checkbutton(self.root, text="Original Image", variable = self.showOriginal, onvalue = True, offvalue = False).grid(row=1, column=0, sticky=tkinter.W, columnspan=2)
+    tkinter.Checkbutton(self.root, text="Patches Plot", variable = self.showPatches, onvalue = True, offvalue = False).grid(row=2, column=0, sticky=tkinter.W, columnspan=2)
   
   def __del__(self):
     self.root.destroy()
   
   def update(self):
     self.root.update()
+  
+  def setPatchesLegend(self, legend):
+    self.patches_legend = legend
+    self.patches_material = []
+
+    def _from_rgb(rgb):
+      (r, g, b) = rgb
+      return '#%02x%02x%02x' % (int(r*255), int(g*255), int(b*255))
+    
+    row_start_index = 3
+    tkinter.Label(self.root, text="Material Patches").grid(row=row_start_index, column=0, sticky=tkinter.W, columnspan=2)
+    row_start_index += 1
+    for p in self.patches_legend:
+      var1 = tkinter.DoubleVar()
+      var1.set(1.0)
+      var2 = tkinter.DoubleVar()
+      var2.set(1.0)
+      self.patches_material.append([var1, var2])
+      tkinter.Label(self.root, text="■", fg=_from_rgb(p[0])).grid(row=row_start_index, column=0, sticky=tkinter.W)
+      tkinter.Label(self.root, text=p[1]).grid(row=row_start_index, column=1, sticky=tkinter.W)
+      row_start_index += 1
+      tkinter.Label(self.root, text="Young's Module").grid(row=row_start_index, column=0, sticky=tkinter.W)
+      tkinter.Entry(self.root, textvariable=var1).grid(row=row_start_index, column=1, sticky=tkinter.W)
+      row_start_index += 1
+      tkinter.Label(self.root, text="Poisson’s ratio").grid(row=row_start_index, column=0, sticky=tkinter.W)
+      tkinter.Entry(self.root, textvariable=var2).grid(row=row_start_index, column=1, sticky=tkinter.W)
+      row_start_index += 1
+
+
+class MeshGenerator:
+  def __init__(self, path):
+    print("[Mesh Generator] reading file")
+    src = cv2.imread(path)
+    image = cv2.flip(src, 0)
+    self.width = image.shape[1]
+    self.height = image.shape[0]
+
+    meshScale = min(image.shape[0], image.shape[1]) / SCALE
+
+    print("[Mesh Generator] binarize image")
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
+
+    print("[Mesh Generator] extracting contours")
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_TC89_L1)
+
+    print("[Mesh Generator] adding geometry")
+    gmsh.initialize()
+    gmsh.model.add("model")
+
+    factory = gmsh.model.geo
+    cts = []
+
+    for c, info in zip(contours, hierarchy[0]):
+        if info[3] == -1:
+            cts.append(None)
+            continue
+        cts.append(Contour(factory, c, info, meshScale))
+
+    for ct in cts:
+        if ct is not None:
+            ct.makePlane(factory, cts)
+
+    print("[Mesh Generator] generating mesh")
+    factory.synchronize()
+    gmsh.model.mesh.generate(2)
+
+    print("[Mesh Generator] writing mesh")
+    gmsh.write("mesh.msh")
+    gmsh.finalize()
+    time.sleep(0.1)
+
+    print("[Mesh Analyzer] reading mesh")
+
+    mesh = meshio.read("mesh.msh")
+
+    self.points = mesh.points
+    self.cells = mesh.cells
+    self.point_data = mesh.point_data
+    self.cell_data = mesh.cell_data
+
+    self.eles_list = []
+    self.nodes_list = []
+    self.loads_list = []
+    self.mater_list = []
+
+    cmap = get_cmap("tab20")
+    self.colors = cmap.colors
+
+    print("[Mesh Analyzer] writing nodes")
+    for i, p in enumerate(self.points):
+      self.nodes_list.append([i, p[0], p[1], 0, 0]) 
+    
+    print("[Mesh Analyzer] writing and plotting elements")
+    self.plotPatches()
+    
+  
+  def plotPatches(self):
+    fig = pylab.figure(figsize=[self.width / 40, self.height / 40], dpi=40)
+    ax = fig.gca()
+
+    self.patch_list = []
+    index = 0
+    element_index = 0
+
+    self.patches_plot_legend = []
+
+    for cell in self.cells:
+        if cell.type == "triangle":
+            self.patch_list.append([])
+            legend_logged = False
+            for pt in cell.data:
+                ptlt = [self.points[pt[0]][0:2].tolist(), self.points[pt[1]][0:2].tolist(), self.points[pt[2]][0:2].tolist()]
+                color = self.colors[index % len(self.colors)]
+                t = plt.Polygon(ptlt, color = color)
+
+                if not legend_logged:
+                  self.patches_plot_legend.append([color, f"Material {index + 1}"])
+                  legend_logged = True
+
+                ax.add_patch(t)
+                self.patch_list[-1].append(copy.deepcopy(ptlt))
+
+                # write element
+                mean = np.mean(ptlt, axis=0)
+                angles = np.arctan2((ptlt-mean)[:, 1], (ptlt-mean)[:, 0])
+                angles[angles < 0] = angles[angles < 0] + 2 * np.pi
+                sorting_indices = np.argsort(angles)
+
+                self.eles_list.append([element_index, 3, index, pt[sorting_indices[0]], pt[sorting_indices[1]], pt[sorting_indices[2]]])
+                element_index += 1
+
+            index += 1
+    
+    ax.axis(xmin=0, xmax=self.width, ymin=0, ymax=self.height)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+
+    canvas = agg.FigureCanvasAgg(fig)
+    canvas.draw()
+    renderer = canvas.get_renderer()
+    self.patches_plot = pygame.image.fromstring(renderer.tostring_rgb(), (self.width, self.height), "RGB")
+    self.patches_plot = pygame.transform.flip(self.patches_plot, False, True)
 
 if __name__ == '__main__':
   pygame.init()
 
-  image = pygame.image.load(prompt_file())
+  path = prompt_file()
+  generator = MeshGenerator(path)
+
+  image = pygame.image.load(path)
   image = pygame.transform.flip(image, False, True)
   size = image.get_rect().size
 
@@ -142,6 +303,7 @@ if __name__ == '__main__':
   canvas = Canvas(size)
 
   gui = UIPanel()
+  gui.setPatchesLegend(generator.patches_plot_legend)
     
   # run window
   running = True
@@ -167,6 +329,8 @@ if __name__ == '__main__':
     canvas.begin()
     if gui.showOriginal.get():
       canvas.getSurface().blit(image, (0, 0))
+    if gui.showPatches.get():
+      canvas.getSurface().blit(generator.patches_plot, (0, 0))
     canvas.end(screen)
 
     pygame.display.update()
--- test.py
+++ test.py
@@ -1,63 +1,15 @@
-import pygame
-from pygame.locals import *
-import tkinter
-import sys   # for exit and arg
+import numpy as np
+import matplotlib.pyplot as pl
 
-def Draw(surf):
-  #Clear view
-  surf.fill((80,80,80))
-  pygame.display.flip()
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
 
+fig = pl.figure(figsize=(8,5))
+ax = pl.gca()
+pl.axis('off')
+pl.plot(x, y)
+pl.xlim(0,10)
+pl.ylim(-1,1)
 
-def GetInput():
-
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      return True
-    if event.type == KEYDOWN:
-      print(event)
-    if event.type == MOUSEBUTTONDOWN:
-      print(event)
-    sys.stdout.flush()  # get stuff to the console
-  return False
-
-
-Done = False
-
-def quit_callback():
-  global Done
-  Done = True
-
-def main():
-
-  # initialise pygame
-  pygame.init()
-  ScreenSize = (200,100)
-  Surface = pygame.display.set_mode(ScreenSize)
-
-  #initialise tkinter
-  root = tkinter.Tk()
-  root.protocol("WM_DELETE_WINDOW", quit_callback)
-  main_dialog =  tkinter.Frame(root)
-  main_dialog.pack()
-
-  # start pygame clock
-  clock = pygame.time.Clock()
-  gameframe = 0
-
-  # main loop
-  while not Done:
-    try:
-      main_dialog.update()
-    except:
-      print("dialog error")
-
-    if GetInput():  # input event can also comes from diaglog
-      break
-    Draw(Surface)
-    clock.tick(100) # slow it to something slightly realistic
-    gameframe += 1
-
-  main_dialog.destroy()
-
-if __name__ == '__main__': main()
+pl.subplots_adjust(left=0, right=1, top=1, bottom=0)
+pl.show()
